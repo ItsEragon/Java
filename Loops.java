404a9ab458fa3d741eb120a8f0f59ec0de554314
Amritpal Singh <amritps2001@gmail.com>
2022-03-09 22:07:58 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+      //ForLoop
+       for (int i = 0; i < 11; i++) {
+           System.out.println(i);
+       }
+
+      //WhileLoop
+       int i = 0;
+       while (i < 11) {
+           System.out.println(i);
+           i++;
+       }
+
+       //DoWhile
+       int j = 0;
+       do {
+           System.out.println(j);
+           j++;
+       } while (j < 11);
+
+    }
+
+}
