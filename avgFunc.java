b451b8c4da33f5b3be0e4e4d653b51040fd65960
Amritpal Singh <amritps2001@gmail.com>
2022-03-09 22:32:53 +0530

    Create avgFunc.java

---
+++ avgFunc.java
@@ -0,0 +1,28 @@
+//Enter 3 numbers from the user & make a function to print their average.
+
+import java.util.*;
+
+public class Main {
+
+    public static void avgfunc(int a, int b, int c) {
+
+        int average = (a + b + c)/2;
+        System.out.println("The average is "+average);
+        return;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter third number: ");
+        int c = sc.nextInt();
+
+        avgfunc(a,b,c);
+    }
+}
